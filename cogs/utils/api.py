bf96f2574dc14b2e94bb971626f205bd0f4b3dca
Sendou <sendou77@gmail.com>
2019-09-12 02:05:40 +0300

    salmon run api query

--- cogs/utils/api.py
+++ cogs/utils/api.py
@@ -6,6 +6,8 @@ class ApiConnecter:
         self.graphql_server_url = "https://www.sendou.ink/graphql"
         self.rotation_data = None
         self.rotation_data_fetch_time = None
+        self.salmon_run_data = None
+        self.salmon_run_data_fetch_time = None
     
     async def request_data(self, url: str) -> dict:
         headers = {
@@ -26,3 +28,13 @@ class ApiConnecter:
             self.rotation_data = await self.request_data("https://splatoon2.ink/data/schedules.json")
             self.rotation_data_fetch_time = current_time
         return self.rotation_data
+    
+    async def get_salmon_run_data(self) -> dict:
+        "Return Salmon Run data from splatoon2.ink as dictionary"
+        current_time = time.time()
+        # Only calling the API if we haven't done so before during this session or it was
+        # more than 2 hours ago.
+        if self.salmon_run_data_fetch_time is None or self.salmon_run_data_fetch_time < current_time - 7200:
+            self.salmon_run_data = await self.request_data("https://splatoon2.ink/data/coop-schedules.json")
+            self.salmon_run_data_fetch_time = current_time
+        return self.salmon_run_data
