4c50f0928f950f294885078c1e100f8c601bbcd8
Sendou <sendou77@gmail.com>
2019-10-26 18:20:04 +0300

    tourneymaps command

--- cogs/tourney.py
+++ cogs/tourney.py
@@ -1,16 +1,18 @@
 import discord
 from discord.ext import commands
 import asyncio
-import challonge
+from math import log, ceil
 
 from .utils import ids
 from .utils import config
+from .utils.map_generator import map_generation
+from .utils.lists import maps
+from .utils.helper import split_to_shorter_parts
 
 
 class TournamentCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.challonge_user = None
 
     async def cog_check(self, ctx):
         """ 
@@ -30,12 +32,6 @@ class TournamentCog(commands.Cog):
                 return True
 
         return False
-
-    async def cog_before_invoke(self, ctx):
-        if self.challonge_user is None:
-            self.challonge_user = await challonge.get_user(
-                config.CHALLONGE_USER_NAME, config.CHALLONGE_API_KEY
-            )
 
     @commands.command(name="part")
     async def change_name_and_give_participant_role(
@@ -77,15 +73,95 @@ class TournamentCog(commands.Cog):
         await member.edit(nick=new_nick)
         await ctx.send(f"Done! New nickname: {new_nick}")
 
-    @commands.command(name="track")
-    async def post_participant_change_of_tournament(self, ctx):
+    @commands.command(name="tourneymaps")
+    async def generate_and_post_maps_for_tournament(
+        self, ctx, amount_of_teams: int, ruleset: str = "ITZ"
+    ):
         """
-        Tracks the tournament given and posts participants joining
-        or leaving to channel.
+        Bot generates a list of maps to use for tournament
+        and says it.
         """
-        tourneys = await self.challonge_user.get_tournaments("sendous")
-        for participant in await tourneys[0].get_participants():
-            print(participant.challonge_username)
+        events = {"ITZ": {"map_pool": {"sz": maps.copy()}}}
+        if ruleset.upper() not in events:
+            return await ctx.send(f"{ruleset} is not a valid ruleset.")
+
+        L2 = log(amount_of_teams, 2)
+        # Winners includes grand finals
+        winner_map_amount = ceil(L2) + 1
+        loser_map_amount = ceil(L2) + ceil(log(L2, 2)) - 1
+
+        games = []
+        games_enum = []
+        if ruleset == "ITZ":
+            games.extend([3, 3, 3])
+            games_enum.extend(["WINNER", "LOSER", "WINNER"])
+            winner_map_amount -= 2
+            loser_map_amount -= 1
+            while winner_map_amount > 0 or loser_map_amount > 0:
+                if winner_map_amount == 1:
+                    games.extend([7, 7])
+                    games_enum.extend(["WINNER", "WINNER"])
+                    winner_map_amount -= 1
+                elif winner_map_amount > 0:
+                    games.append(5)
+                    games_enum.append("WINNER")
+                    winner_map_amount -= 1
+
+                if loser_map_amount == 1:
+                    games.append(5)
+                    games_enum.append("LOSER")
+                    loser_map_amount -= 1
+                elif loser_map_amount > 0:
+                    games.append(3)
+                    games_enum.append("LOSER")
+                    loser_map_amount -= 1
+
+        generated_maps = map_generation(
+            map_pool=events[ruleset]["map_pool"], games=games
+        )
+
+        winner_map_str = "__**WINNERS**__\n\n"
+        losers_map_str = "__**LOSERS**__\n\n"
+
+        winner_map_count = ceil(L2) + 2
+        losers_round = 1
+        winners_round = 1
+        print_modes = ruleset.upper() not in ["ITZ", "DRAFT"]
+        for map_tuple in list(zip(games_enum, generated_maps)):
+            enum, maplist = map_tuple
+
+            if enum == "LOSER":
+                losers_map_str += f"Losers Round {losers_round}\n```"
+                for count, stage_tuple in enumerate(maplist, 1):
+                    mode, stage = stage_tuple
+                    mode = f"{mode} on " if print_modes else ""
+                    losers_map_str += f"{count}) {mode}{stage}\n"
+                losers_map_str += "```\n"
+                losers_round += 1
+            else:
+                title = f"Round {winners_round}"
+                if winner_map_count == 3:
+                    title = "Semifinals"
+                elif winner_map_count == 2:
+                    title = "Finals"
+                elif winner_map_count == 1:
+                    title = "Finals (Bracket Reset)"
+                winner_map_str += f"{title}\n```"
+                for count, stage_tuple in enumerate(maplist, 1):
+                    mode, stage = stage_tuple
+                    mode = f"{mode} on " if print_modes else ""
+                    winner_map_str += f"{count}) {mode}{stage}\n"
+                winner_map_str += "```\n"
+                winners_round += 1
+                winner_map_count -= 1
+
+        before_all = (
+            "Mode: Splat Zones\n\n" if ruleset.upper() in ["ITZ", "DRAFT"] else ""
+        )
+        for line in split_to_shorter_parts(
+            before_all + winner_map_str + losers_map_str
+        ):
+            await ctx.send(line)
 
 
 def setup(bot):
